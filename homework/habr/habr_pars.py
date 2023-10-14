75ca71d96fc672b6a9200ceca2754ea6c22f63d5
Rusik <143873668+parysnik@users.noreply.github.com>
2023-10-14 10:38:57 +0300

    upd

--- homework/habr/habr_pars.py
+++ homework/habr/habr_pars.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-#ссылка
+
 url = "https://habr.com/ru/articles/top/weekly/"
 
-#ответ и объект супа и поиск тегов h2
+
 r = requests.get(url)
 soup = BS(r.text, 'html.parser')
 h2_tegs = soup.find_all('h2')
@@ -19,13 +19,13 @@ for h2 in h2_tegs:
         if not art_url.startswith('http'):
             art_url = 'https://habr.com' + art_url
 
-        #цифры для названия
+
         art_numb = art_url.split('/')[-2]
 
-        #статья для файлика
+
         art_r = requests.get(art_url)
 
-        #в файлик
+
         with open(f'{art_numb}.html', 'w', encoding='utf-8') as file:
             file.write(art_r.text)
     else:
